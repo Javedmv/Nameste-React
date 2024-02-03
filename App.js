cdd4718c503698f17bb01247c9374ed41fe29520
Javedmv <javedmv777@gmail.com>
2024-02-04 01:35:45 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1",{id:"outer"},"heading1");
+const heading1 = React.createElement("h2",{id:"inner"},"Nameste Everyone");
+const root = ReactDOM.createRoot(document.getElementById("root"))
+// console.log(heading);
+const container = React.createElement("div",{id:"container"},[heading,heading1])
+root.render(container)
