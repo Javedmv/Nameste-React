0687c4559b08ba3573ad4bcc964eafda48bc7088
Javedmv <javedmv777@gmail.com>
2024-02-06 00:13:57 +0530

    ignite the app

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
 const heading = React.createElement("h1",{id:"outer"},"heading1");
 const heading1 = React.createElement("h2",{id:"inner"},"Nameste Everyone");
 const root = ReactDOM.createRoot(document.getElementById("root"))
