de2f4cc638b22b3866491bb00c25f8d187624727
Javedmv <javedmv777@gmail.com>
2024-04-02 13:00:06 +0530

    ep-4

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
-const heading = React.createElement("h1",{id:"outer"},"heading1");
-const heading1 = React.createElement("h2",{id:"inner"},"Nameste Everyone");
+import "./index.css"
+
+const NavBar = () => {
+    return (
+        <div className="header">
+          <div className="logo">Logo</div>
+          <div className="search-bar">
+            <input type="text" placeholder="Search..." />
+            <button>Search</button>
+          </div>
+          <div className="user-icon">User</div>
+        </div>
+      );
+};
+
+// const heading1 = React.createElement("h2",{id:"inner",key:"h2"},"Nameste Everyone");
 const root = ReactDOM.createRoot(document.getElementById("root"))
 // console.log(heading);
-const container = React.createElement("div",{id:"container"},[heading,heading1])
-root.render(container)
+// const container = React.createElement("div",{id:"container"},<NavBar />)
+root.render(<NavBar />)
+
